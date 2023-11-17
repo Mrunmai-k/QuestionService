93a0401de2098c4826295172dd893fdbb3c7e93e
mrunmai <mrunmaikudale18@gmail.com>
2023-11-17 20:53:01 +0530

    Checked the server port to handle load balancing by feign and eureka client

--- src/main/java/com/mrunmai/questionservice/controller/QuestionController.java
+++ src/main/java/com/mrunmai/questionservice/controller/QuestionController.java
@@ -5,6 +5,7 @@ import com.mrunmai.questionservice.model.QuestionWrapper;
 import com.mrunmai.questionservice.model.Response;
 import com.mrunmai.questionservice.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +18,8 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
 
+    @Autowired
+    Environment environment;
     @GetMapping("allQuestions")
     public ResponseEntity<List<Question>> getAllQuestions(){
         return questionService.getAllQuestions();
@@ -39,6 +42,7 @@ public class QuestionController {
 
     @PostMapping("getQuestions")
     public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestBody List<Integer> questionIds){
+        System.out.println(environment.getProperty("local.server.port"));
         return questionService.getQuestionsFromId(questionIds);
     }
 
